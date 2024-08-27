38d47638f12281b9f63179efe262aee09c85045b
iamsyahirah <syahirahmohammad89@gmail.com>
2024-08-27 21:50:23 +0800

    middleware guest

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
@@ -21,9 +22,10 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'authenticating']);
-Route::get('register', [AuthController::class, 'register']);
+Route::get('login', [AuthController::class, 'login'])->name('login')->middleware('only_guest');
+Route::post('login', [AuthController::class, 'authenticating'])->middleware('only_guest');
+Route::get('register', [AuthController::class, 'register'])->middleware('only_guest');
 
 Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'only_admin']);
 Route::get('profile', [UserController::class, 'profile'])->middleware(['auth', 'only_client']);
+Route::get('books', [BookController::class, 'index'])->middleware('auth');
