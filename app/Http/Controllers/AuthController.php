73175cbb9fb31bd9bb21abe51402c15b57f42ab1
iamsyahirah <syahirahmohammad89@gmail.com>
2024-08-27 17:46:46 +0800

    check status user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login()
     {
         return view('login');
     }
@@ -14,5 +16,28 @@ class AuthController extends Controller
     public function register()
     {
         return view('register');
+    }
+
+    public function authenticating(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+
+            // check status = active
+            if (Auth::user()->status != 'active') {
+                Session::flash('status', 'failed');
+                Session::flash('message', 'Your account is not active yet. Please contact admin.');
+                return redirect('/login');
+            }
+
+            //return redirect()->intended('dashboard');
+        }
+        Session::flash('status', 'failed');
+        Session::flash('message', 'Login Invalid');
+        return redirect('/login');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,17 +24,22 @@
     }
 </style>
 <body>
-    <div class="main d-flex justify-content-center align-items-center">
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+       @if (session('status'))
+    <div class="alert alert-danger">
+        {{ session('message')}}
+    </div>
+       @endif
         <div class="login-box">
             <form action="" method="post">
                 @csrf
                 <div>
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username" class="form-control">
+                    <input type="text" name="username" id="username" class="form-control" required>
                 </div>
                 <div>
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" name="username" id="password" class="form-control">
+                    <input type="password" name="password" id="password" class="form-control" required>
                 </div>
                 <div>
                     <button type="submit" class="btn btn-primary form-control">Login</button>
@@ -46,12 +51,6 @@
 
         </div>
     </div>
-
-
-
-
-
-
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
