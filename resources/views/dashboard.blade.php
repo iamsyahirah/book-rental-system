c96b80c2dd430237df0c1239d36014d365a190c3
iamsyahirah <syahirahmohammad89@gmail.com>
2024-08-28 23:44:25 +0800

    style: layout flex

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,5 @@
 @section('title','Dashboard')
 
 @section('content')
-    <button class="btn btn-primary">Testing</button>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur exercitationem perferendis autem est maiores aut. Rerum hic, veritatis, aperiam quod qui voluptas ullam a quaerat consectetur enim accusantium debitis porro quae fugit temporibus deserunt repudiandae incidunt id maxime magni molestias cum earum inventore animi. Nihil eligendi libero tempore officiis, sequi corrupti nemo at doloribus. Quod praesentium ullam animi exercitationem laboriosam iusto dicta eum ab explicabo, quam magni numquam voluptatum? Atque officia, at eaque iusto vero consectetur libero exercitationem, provident enim ut nisi sint repellendus inventore eligendi. Blanditiis eum rerum recusandae odio commodi quasi suscipit earum similique sunt. Laborum, praesentium nostrum.</p>
 @endsection
--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -6,8 +6,47 @@
     <title>Rental Book System | @yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
+  <style>
+    .main {
+        height: 100vh;
+    }
+
+    .sidebar {
+      background:#232227 ;
+      color: #fff;
+    }
+
+    .content {
+      background: #f3f7ff;
+    }
+  </style>
   <body>
-    <div>@yield('content')</div>
+    <div class="main d-flex flex-column justify-content-between">
+        <nav class="navbar navbar-expand-lg bg-light shadow-lg p-3 bg-body rounded">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#">Rental Book System</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+  
+    </div>
+        </nav>
+
+  <div class="body-content h-100">
+    <div class="row g-0 h-100">
+      <div class="sidebar col-2">
+        gggg
+      </div>
+      <div class="content p-5 col-10">
+         @yield('content')
+      </div>
+    </div>
+  </div>
+    </div>
+
+   
+
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
