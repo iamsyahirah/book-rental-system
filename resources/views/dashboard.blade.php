7b2cd4d054a610bccc7d33440b9f132e34ad246b
iamsyahirah <syahirahmohammad89@gmail.com>
2024-08-28 22:53:52 +0800

    style: layout dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-</head>
-<body>
-    <h1>This is Dashboard</h1>
-    
-</body>
-</html>
+@extends('layouts.mainlayout')
+
+@section('title','Dashboard')
+
+@section('content')
+    <button class="btn btn-primary">Testing</button>
+@endsection
