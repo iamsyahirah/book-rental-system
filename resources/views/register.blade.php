9968b748c20ae9e36257a96d5f0b3ab049800478
iamsyahirah <syahirahmohammad89@gmail.com>
2024-08-28 14:12:49 +0800

    design form register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,11 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Book Rental System | Register</title>
 </head>
+<style>
+     .main {
+        height: 100vh;
+        box-sizing: border-box;
+    }
+
+    .register-box {
+        width: 500px;
+        border: solid 1px;
+        padding: 30px;
+    }
+
+    form div {
+        margin-bottom: 20px;
+    }
+</style>
 <body>
-    <h1>Register Page</h1>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+     <div class="main d-flex flex-column justify-content-center align-items-center">
+       @if (session('status'))
+    <div class="alert alert-danger">
+        {{ session('message')}}
+    </div>
+       @endif
+        <div class="register-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control" required>
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div>
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" name="phone" id="phone" class="form-control" required>
+                </div>
+                <div>
+                    <label for="address" class="form-label">Address</label>
+                   <textarea name="address" id="address" class="form-control"></textarea>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Register</button>
+                </div>
+                <div class="text-center">
+                    <a href="login">Login</a>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
