4c5753cb8871b0ee3f0eed192a711e0cec13d042
iamsyahirah <syahirahmohammad89@gmail.com>
2024-08-30 22:37:58 +0800

    feat: show categories from db

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,5 +5,29 @@
 @section('page-name','Categories')
 
 @section('content')
-  <h1>Category page</h1>
+  <h1>Category List</h1>
+
+  <div class="my-5">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($categories as $item)
+        <tr>
+          <td>{{ $loop->iteration}}</td>
+          <td>{{ $item->name}}</td>
+          <td>
+            <a href="#">Edit</a>
+            <a href="#">Delete</a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
