ef52d2dc61ca962fac1a14215f11e4446bdb53a0
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-05 16:05:52 +0800

    error: image upload

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -22,10 +22,38 @@ class BookController extends Controller
     {
         $validated = $request->validate([
             'book_code' => 'required|unique:books|max:255',
-            'title' => 'required|max:255'
+            'title' => 'required|max:255',
+
         ]);
 
-        $book = Book::create($request->all());
+        $newName = '';
+
+        // Check if the request has a file for 'image'
+        if ($request->hasFile('image')) {
+            $extension = $request->file('image')->getClientOriginalExtension();
+            $newName = $request->title . '-' . now()->getTimestamp() . '.' . $extension;
+            $request->file('image')->storeAs('cover', $newName);
+        }
+
+        // Merge the validated data with the new 'cover' field
+        $data = array_merge($validated, ['cover' => $newName]);
+
+        // Create a new book record with the combined data
+        $book = Book::create($data);
+
+        // Redirect back with success message
         return redirect('books')->with('status', 'Book Added Successfully');
+
+
+        //  if ($request->file('image')) {
+        //      $extension = $request->file('image')->getClientOriginalExtension();
+        //      $newName = $request->title . '-' . now()->timestamp . '.' . $extension;
+        //     $request->file('image')->storeAs('cover', $newName);
+        //  }
+
+        // $request['cover'] = $newName;
+
+        //  $book = Book::create($request->all());
+        //   return redirect('books')->with('status', 'Book Added Successfully');
     }
 }
--- resources/views/book-add.blade.php
+++ resources/views/book-add.blade.php
@@ -20,17 +20,17 @@
       @csrf
       <div class="mb-3" enctype="multipart/form-data">
         <label for="book_code" class="form-label">Code</label>
-        <input type="text" name="book_code" id="book_code" class="form-control" placeholder="Book Code" value="{{ old('book_code')}}">
+        <input type="text" name="book_code" id="book_code" class="form-control" placeholder="Book's Code" value="{{ old('book_code')}}">
       </div>
 
        <div class="mb-3">
         <label for="title" class="form-label">Title</label>
-        <input type="text" name="title" id="title" class="form-control" placeholder="Book Title" value="{{ old('title')}}">
+        <input type="text" name="title" id="title" class="form-control" placeholder="Book's Title" value="{{ old('title')}}">
       </div>
 
        <div class="mb-3">
-        <label for="cover" class="form-label">Cover</label>
-        <input type="file" name="cover" id="cover" class="form-control">
+        <label for="image" class="form-label">Image</label>
+        <input type="file" name="image" id="image" class="form-control">
       </div>
 
 
