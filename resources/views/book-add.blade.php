f00801afc33895fd72df4b4fb0041d35a7687edf
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-12 11:56:01 +0800

    feat: add select2 jquery

--- resources/views/book-add.blade.php
+++ resources/views/book-add.blade.php
@@ -3,6 +3,9 @@
 @section('title','Add Book')
 
 @section('content')
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
   <h1>Add New Book</h1>
 
   @if ($errors->any())
@@ -35,8 +38,7 @@
 
       <div class="mb-3">
         <label for="category" class="form-label">Category</label>
-        <select name="categories" id="category" class="form-control">
-          <option value="">Choose Category</option>
+        <select name="categories[]" id="category" class="form-control select-multiple" multiple>
          @foreach ($categories as $item)
          <option value="{{ $item->id }}">{{ $item->name }}</option>
          @endforeach
@@ -50,4 +52,12 @@
       </div>
     </form>
   </div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> 
+<script>
+  $(document).ready(function() {
+    $('.select-multiple').select2();
+});
+</script>
 @endsection
