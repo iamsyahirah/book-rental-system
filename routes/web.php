81032ce323619c2faf5c6c5fa5ba2ffaed481c95
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-20 00:08:15 +0800

    update: routes, middleware

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('auth');
+});
 
 Route::middleware('only_guest')->group(function () {
     Route::get('login', [AuthController::class, 'login'])->name('login');
@@ -34,40 +34,39 @@ Route::middleware('only_guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('logout', [AuthController::class, 'logout']);
-    Route::get('dashboard', [DashboardController::class, 'index'])->middleware('only_admin');
-
     Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
 
-    Route::get('books', [BookController::class, 'index']);
-    Route::get('book-add', [BookController::class, 'add']);
-    Route::post('book-add', [BookController::class, 'store']);
+    Route::middleware('only_admin')->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index']);
+        Route::get('books', [BookController::class, 'index']);
+        Route::get('book-add', [BookController::class, 'add']);
+        Route::post('book-add', [BookController::class, 'store']);
+        Route::get('book-edit/{slug}', [BookController::class, 'edit']);
+        Route::post('book-edit/{slug}', [BookController::class, 'update']);
+        Route::get('book-delete/{slug}', [BookController::class, 'delete']);
+        Route::get('book-destroy/{slug}', [BookController::class, 'destroy']);
+        Route::get('book-deleted', [BookController::class, 'deletedBook']);
+        Route::get('book-restore/{slug}', [BookController::class, 'restore']);
 
-    Route::get('book-edit/{slug}', [BookController::class, 'edit']);
-    Route::post('book-edit/{slug}', [BookController::class, 'update']);
-    Route::get('book-delete/{slug}', [BookController::class, 'delete']);
-    Route::get('book-destroy/{slug}', [BookController::class, 'destroy']);
-    Route::get('book-deleted', [BookController::class, 'deletedBook']);
-    Route::get('book-restore/{slug}', [BookController::class, 'restore']);
+        Route::get('categories', [CategoryController::class, 'index']);
+        Route::get('category-add', [CategoryController::class, 'add']);
+        Route::post('category-add', [CategoryController::class, 'store']);
+        Route::get('category-edit/{slug}', [CategoryController::class, 'edit']);
+        Route::put('category-edit/{slug}', [CategoryController::class, 'update']);
+        Route::get('category-delete/{slug}', [CategoryController::class, 'delete']);
+        Route::get('category-destroy/{slug}', [CategoryController::class, 'destroy']);
+        Route::get('category-deleted', [CategoryController::class, 'deleteCategory']);
+        Route::get('category-restore/{slug}', [CategoryController::class, 'restore']);
 
-
-    Route::get('categories', [CategoryController::class, 'index']);
-    Route::get('category-add', [CategoryController::class, 'add']);
-    Route::post('category-add', [CategoryController::class, 'store']);
-    Route::get('category-edit/{slug}', [CategoryController::class, 'edit']);
-    Route::put('category-edit/{slug}', [CategoryController::class, 'update']);
-    Route::get('category-delete/{slug}', [CategoryController::class, 'delete']);
-    Route::get('category-destroy/{slug}', [CategoryController::class, 'destroy']);
-    Route::get('category-deleted', [CategoryController::class, 'deleteCategory']);
-    Route::get('category-restore/{slug}', [CategoryController::class, 'restore']);
-
-    Route::get('users', [UserController::class, 'index']);
-    Route::get('registered-users', [UserController::class, 'registeredUser']);
-    Route::get('user-detail/{slug}', [UserController::class, 'show']);
-    Route::get('user-approve/{slug}', [UserController::class, 'approve']);
-    Route::get('user-ban/{slug}', [UserController::class, 'delete']);
-    Route::get('user-destroy/{slug}', [UserController::class, 'destroy']);
-    Route::get('user-banned', [UserController::class, 'bannedUser']);
-    Route::get('user-restore/{slug}', [UserController::class, 'restore']);
+        Route::get('users', [UserController::class, 'index']);
+        Route::get('registered-users', [UserController::class, 'registeredUser']);
+        Route::get('user-detail/{slug}', [UserController::class, 'show']);
+        Route::get('user-approve/{slug}', [UserController::class, 'approve']);
+        Route::get('user-ban/{slug}', [UserController::class, 'delete']);
+        Route::get('user-destroy/{slug}', [UserController::class, 'destroy']);
+        Route::get('user-banned', [UserController::class, 'bannedUser']);
+        Route::get('user-restore/{slug}', [UserController::class, 'restore']);
+    });
 
     Route::get('rent-logs', [RentLogController::class, 'index']);
 });
