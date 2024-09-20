2a540d24acf562e52e676b9734eb604ae5156d68
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-20 12:54:13 +0800

    style: design book list

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -4,5 +4,28 @@
 
 @section('content')
 
+<div class="my-5">
+    <div class="row">
+        <div class="col-3 mb-3">
+             <div class="card">
+                <img src="{{ asset('images/cover_not_found.jpeg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+       
+
+       
+
+      
+
+      
+    </div>
+</div>
+
     
 @endsection
