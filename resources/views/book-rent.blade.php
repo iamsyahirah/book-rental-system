10f9928c696971ab9e47f70af15796da980f18e2
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-22 13:16:17 +0800

    style: rent form

--- resources/views/book-rent.blade.php
+++ resources/views/book-rent.blade.php
@@ -4,6 +4,22 @@
 
 @section('content')
 
-  
+  <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-md-3">
+    <h1>Book Rent Form</h1>
+    <form action="" method="post">
+        <div class="mb-3">
+            <label for="user" class="form-label">User</label>
+            <select name="user" id="user" class="form-control"></select>
+        </div>
+         <div class="mb-3">
+            <label for="book" class="form-label">Book</label>
+            <select name="book" id="book" class="form-control"></select>
+        </div>
+        <div>
+            <button class="btn btn-primary w-100" type="submit">Submit</button>
+        </div>
+
+    </form>
+  </div>
     
 @endsection
--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -27,21 +27,18 @@
       <div class="sidebar col-lg-2 collapse d-lg-block" id="navbarSidebar">
         @if (Auth::user())
           @if (Auth::user()->role_id == 1)
-          <a href="/dashboard" 
-          @if(request()->route()->uri == 'dashboard') class="active" 
-          @endif>
-            <i class="fa-solid fa-gauge icon-spacing"></i></i>
-            Dashboard</a>
-          <a href="/books"
-          @if(request()->route()->uri == 'books' || request()->route()->uri == 'book-add' || request()->route()->uri == 'book-deleted' || request()->route()->uri == 'book-edit/{slug}' || request()->route()->uri == 'book-delete/{slug}') class="active"
-          @endif>
-            <i class="fa-solid fa-book icon-spacing"></i>Books</a>
-          <a href="/categories"
-          @if(request()->route()->uri == 'categories' || request()->route()->uri == 'category-add' || request()->route()->uri == 'category-deleted' || request()->route()->uri == 'category-edit/{slug}' || request()->route()->uri == 'category-delete/{slug}' ) class="active" 
-          @endif>
-           <i class="fa-solid fa-layer-group icon-spacing"></i>Categories</a>
-           <a href="/" @if(request()->route()->uri == '/') class="active"  @endif><i class="fa-solid fa-book-open icon-spacing"></i>Book List</a>
-            <a href="/book-rent" @if(request()->route()->uri == '/') class="active"  @endif><i class="fa-solid fa-swatchbook icon-spacing"></i>Book Rent</a>
+          <a href="/dashboard" @if(request()->route()->uri == 'dashboard') class="active" 
+          @endif><i class="fa-solid fa-gauge icon-spacing"></i></i>Dashboard</a>
+
+          <a href="/books" @if(request()->route()->uri == 'books' || request()->route()->uri == 'book-add' || request()->route()->uri == 'book-deleted' || request()->route()->uri == 'book-edit/{slug}' || request()->route()->uri == 'book-delete/{slug}') class="active" @endif><i class="fa-solid fa-book icon-spacing"></i>Books</a>
+
+          <a href="/categories" @if(request()->route()->uri == 'categories' || request()->route()->uri == 'category-add' || request()->route()->uri == 'category-deleted' || request()->route()->uri == 'category-edit/{slug}' || request()->route()->uri == 'category-delete/{slug}' ) class="active" 
+          @endif><i class="fa-solid fa-layer-group icon-spacing"></i>Categories</a>
+
+          <a href="/" @if(request()->route()->uri == '/') class="active"  @endif><i class="fa-solid fa-book-open icon-spacing"></i>Book List</a>
+
+          <a href="/book-rent" @if(request()->route()->uri == 'book-rent') class="active"  @endif><i class="fa-solid fa-swatchbook icon-spacing"></i>Book Rent</a>
+
           <a href="/users"
           @if(request()->route()->uri == 'users' || request()->route()->uri == 'registered-users' || request()->route()->uri == 'user-detail/{slug}' || request()->route()->uri == 'user-ban/{slug}' || request()->route()->uri == 'user-banned') class="active" 
           @endif>
