60b95f86bf5eb9378bd5ca44aa37c2e51fb65bb5
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-22 10:32:53 +0800

    style: form search, category

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -4,6 +4,28 @@
 
 @section('content')
 
+<form action="" method="get">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <select name="category" id="category" class="form-control">
+                <option value="">Select Category</option>
+                @foreach ($categories as $item)
+                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-12 col-sm-6">
+            <div class="input-group mb-3">
+  <input type="text" name="title" class="form-control" placeholder="Search Book's Title" >
+  <div class="input-group-append">
+    <button class="btn btn-primary" type="submit">Search</button>
+  </div>
+</div>
+
+        </div>
+    </div>
+</form>
+
 <div class="my-5">
     <div class="row">
         @foreach ($books as $item)
