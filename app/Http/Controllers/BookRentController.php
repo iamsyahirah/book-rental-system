f454100f45e2ce935e5ae7a22c518d6d8642e5ff
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-23 01:03:44 +0800

    feat: save data borrower

--- app/Http/Controllers/BookRentController.php
+++ app/Http/Controllers/BookRentController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\Book;
 use App\Models\User;
+use App\Models\RentLogs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class BookRentController extends Controller
@@ -28,9 +30,29 @@ class BookRentController extends Controller
             Session::flash('message', 'Cannot rent, the book is not available');
             Session::flash('alert-class', 'alert-danger');
             return redirect('book-rent');
+        } else {
+            try {
+                DB::beginTransaction();
+                //process insert to rent_logs table
+                RentLogs::create($request->all());
+                //process update book table
+                $book = Book::findOrFail($request->book_id);
+                $book->status = 'not available';
+                $book->save();
+                DB::commit();
+
+                Session::flash('message', 'Rent Book Success');
+                Session::flash('alert-class', 'alert-success');
+                return redirect('book-rent');
+
+            } catch (\Throwable $th) {
+                DB::rollBack();
+                dd($th);
+            }
+
         }
 
-        dd('boleh pinjam');
+
 
 
     }
