5dad2c168c2f7fad137888068d673d33f5c3d889
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-22 13:58:23 +0800

    feat: get data from db

--- app/Http/Controllers/BookRentController.php
+++ app/Http/Controllers/BookRentController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BookRentController extends Controller
 {
     public function index()
     {
-        return view('book-rent');
+        $users = User::where('id', '!=', 1)->get();
+        $books = Book::all();
+        return view('book-rent', ['users' => $users, 'books' => $books]);
     }
 }
--- resources/views/book-rent.blade.php
+++ resources/views/book-rent.blade.php
@@ -9,11 +9,21 @@
     <form action="" method="post">
         <div class="mb-3">
             <label for="user" class="form-label">User</label>
-            <select name="user" id="user" class="form-control"></select>
+            <select name="user" id="user" class="form-control">
+                <option value="">Select User</option>
+                @foreach ($users as $item)
+                <option value="{{ $item->id }}">{{ $item->username }}</option>
+                @endforeach
+            </select>
         </div>
          <div class="mb-3">
             <label for="book" class="form-label">Book</label>
-            <select name="book" id="book" class="form-control"></select>
+            <select name="book" id="book" class="form-control">
+                <option value="">Select Book</option>
+                @foreach ($books as $item)
+                <option value="{{ $item->id }}">{{ $item->title }}</option>
+                @endforeach
+            </select>
         </div>
         <div>
             <button class="btn btn-primary w-100" type="submit">Submit</button>
